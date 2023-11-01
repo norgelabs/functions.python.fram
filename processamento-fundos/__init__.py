ec856b26c8bda3af85e661a19a6168fc9977ae19
ivoschettini <124700515+ivoschettini@users.noreply.github.com>
2023-11-01 16:09:29 -0300

    Update __init__.py

--- processamento-fundos/__init__.py
+++ processamento-fundos/__init__.py
@@ -12,7 +12,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     connect_str = "DefaultEndpointsProtocol=https;AccountName=functionspythonfram8ffb;AccountKey=a1vrSV+WzPqf0PLv7RlNtRsb0JpOsNOHsTCDuLKoxrh69dIDstM1WgY5ydR5ViyrVgPFPfCxe0a4+ASt+TK9XA==;EndpointSuffix=core.windows.net"
 
-    req_body = req.get_body()
+    req_body = str(req.get_body())
 
     logging.info(req_body)
     
@@ -24,11 +24,11 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     queue_client.message_encode_policy = BinaryBase64EncodePolicy()
     
     message_bytes = req_body.encode('utf-8')
-    base64_message = base64.b64encode(message_bytes).decode('utf-8')
+    # base64_message = base64.b64encode(message_bytes).decode('utf-8')
 
-    queue_client.send_message(base64_message)
+    # queue_client.send_message(base64_message)
 
-    # queue_client.send_message(queue_client.message_encode_policy.encode(content=message_bytes))
+    queue_client.send_message(queue_client.message_encode_policy.encode(content=message_bytes))
     
     return func.HttpResponse(
             "This HTTP triggered function executed successfully",
