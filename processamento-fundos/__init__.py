ea591b584c67115827995bfe3e83252cf3938c0d
ivoschettini <124700515+ivoschettini@users.noreply.github.com>
2023-11-01 15:52:17 -0300

    Update __init__.py

--- processamento-fundos/__init__.py
+++ processamento-fundos/__init__.py
@@ -3,6 +3,7 @@ import logging
 import os, uuid
 from azure.identity import DefaultAzureCredential
 from azure.storage.queue import QueueServiceClient, QueueClient, QueueMessage, BinaryBase64DecodePolicy, BinaryBase64EncodePolicy
+import base64
 
 import azure.functions as func
 
@@ -11,7 +12,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     connect_str = "DefaultEndpointsProtocol=https;AccountName=functionspythonfram8ffb;AccountKey=a1vrSV+WzPqf0PLv7RlNtRsb0JpOsNOHsTCDuLKoxrh69dIDstM1WgY5ydR5ViyrVgPFPfCxe0a4+ASt+TK9XA==;EndpointSuffix=core.windows.net"
 
-    req_body = req.get_json()
+    req_body = req.get_body()
+
+    logging.info(req_body)
     
     queue_name = "processamento-de-fundos"
     queue_client = QueueClient.from_connection_string(connect_str, queue_name)
